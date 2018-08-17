acaaaac54ae54484bd944ce7cb8c195a085ba8b7
Gavin Soko <sokolofg@gmail.com>
2018-08-17 16:20:18 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,14 @@
   def my_select(selection)
-   if block_given?
+   
     i = 0 
     newArr = []
     while i < selection.length
     if yield(selection[i]) == true
+      newArr << selection[i]
+    end
     i+=1
     end
   newArr
    end
-  end
+  
 
